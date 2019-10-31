b5b06310e764de6968a477a8682270513a2b7e11
zhenya_subbota <zhenya.subbota.19@mail.ru>
2019-10-31 22:17:56 +0300

    rework in program

--- first_win.py
+++ first_win.py
@@ -13,11 +13,10 @@ class Main():
 
 class Design_window(Main):
     def __init__(self,root):
-        self.alfa = (
+        self.alfa = [
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.number = [i + 1 for i in range(26)]
-        self.alfa_number = dict(zip(self.alfa, self.number))
 
         self.name=Label(root, text="Input text:", font="Arial 20").place(x=10,y=10)
         self.input_1 = Entry(root)
@@ -42,14 +41,15 @@ class Button_for_1(Design_window):
         key = self.input_3.get()
         new_name = []
 
+        letter=int(mod)/gcd(int(key),int(mod))
         for i in name:
-            math = self.alfa_number[i] * int(key) % int(mod)
+            math = (self.alfa.index(i)+1) * int(key) % int(mod)
             new_name.append(self.alfa[math - 1])
         new_name = ''.join(new_name)
         Dialog_window(key,mod)
-        Create_add_window(name,new_name,'i=index*key%mod','en')
+        Create_add_window(name,new_name,'i=index*key%mod','en',int(letter))
 
-class Button_for_2(Main):
+class Button_for_2(Design_window):
     def __init__(self, root):
         Design_window.__init__(self, root)
         button_1 = Button(root, text='Execute', font="Arial 22", command=self.check, width=12,
@@ -63,14 +63,17 @@ class Button_for_2(Main):
         key = self.input_3.get()
         old_name = []
 
+
         for i in name:
             for j in self.number:
-                math_var = j * int(key) % int(mod)
-                if math_var == self.alfa_number[i]:
+                letter = j * int(key) % int(mod)
+                if letter == self.alfa.index(i)+1:
                     old_name.append(self.alfa[j - 1])
+                    break
+
         old_name = ''.join(old_name)
         Dialog_window(key,mod)
-        Create_add_window(name,old_name, 'use cycle','de')
+        Create_add_window(name,old_name, '(i+mod-k)% mod','de')
 
 
 class Dialog_window():
@@ -86,14 +89,27 @@ class Dialog_window():
 
 
 class Create_add_window():
-    def __init__(self,start_name,after_name,formula,word):
-        new_window=Tk()
-        new_window.geometry("400x250")
-        new_window.title("Add_window")
+ def __init__(self,start_name,after_name,formula,word,letter=None):
+    new_window=Tk()
+    new_window.geometry("500x250")
+    new_window.title("Add_window")
 
-        outpt_1 = Label(new_window, text=str(formula), font="Arial 20").place(x=150, y=10)
-        outpt_2= Label(new_window, text="Start text:" + str(start_name), font="Arial 20").place(x=10, y=40)
-        outpt_3=Label(new_window, text="Text after "+str(word)+ "cryption:"+str(after_name), font="Arial 20").place(x=10,y=100)
+    outpt_1 = Label(new_window, text=str(formula), font="Arial 20").place(x=150, y=10)
+
+    outpt_2 = Label(new_window, text="Start text:", font="Arial 20").place(x=10, y=60)
+    outpt_3 = Label(new_window, text="Text after " + str(word) + "cryption:", font="Arial 20").place(x=10, y=100)
+
+    outpt_4 = Label(new_window, text="Letter:" + str(letter),
+                    font="Arial 20").place(x=10, y=140)
+
+    input_1 = Entry(new_window)
+    input_1.place(x=135, y=70)
+    input_1.insert(1,start_name)
+
+    input_2 = Entry(new_window)
+    input_2.place(x=270, y=115)
+    input_2.insert(1,after_name)
+
 
 if __name__=="__main__":
     root_1=Tk()
